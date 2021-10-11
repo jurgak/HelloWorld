1be53e29ae230a35af8822592f2ec4f6336efd19
Inta.Slapna <inta.slapnja@inbox.lv>
2021-10-11 21:25:49 +0300

    Added branch and method Inta

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Inta();
 
     }
+
+    public static void Inta (){
+        System.out.println("Inta says Hello World!");
+    }
+
+
 }
