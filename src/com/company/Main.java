c77e2c3599b81fecbcfca5a411f5c388f670cc65
Jelena <jelenbol@gmail.com>
2021-10-11 21:27:59 +0300

    Jelena's commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        jelena();
     }
+        public static void jelena ()
+        {
+            System.out.println("Jelena says Hello World!");
+
+        }
+
 }
+
+
+
